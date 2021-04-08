bcc7308a9a2ba585252eead735fda399a98f9ae5
somesh12345 <81948764+somesh12345@users.noreply.github.com>
2021-04-08 09:44:36 +0530

    jj
    uktyur

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hospital Managment</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="header" id="top-header">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container text-uppercase p-2">
+				<a class="navbar-brand" href="#">Hospital Managment</a>
+  				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 	aria-expanded="false" aria-label="Toggle navigation">
+    			<span class="navbar-toggler-icon"></span>
+  				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    				<ul class="navbar-nav ml-auto">
+      					<li class="nav-item active">
+        					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      					</li>
+      					<li class="nav-item active">
+        					<a class="nav-link" href="#">About Us</a>
+      					</li>
+      					<li class="nav-item active">
+        					<a class="nav-link" href="#">Services</a>
+      					</li>
+      					<li class="nav-item active">
+        					<a class="nav-link" href="#">Contact Us</a>
+      					</li>
+      					<li class="nav-item active">
+        					<a class="nav-link" href="#">Register</a>
+      					</li>
+  				</div>
+  			</div>
+		</nav>
+    <section class="header-section">
+      <div class="center-div">
+        <h1 class="font-weight-bold"></h1>
+        <p></p>
+        <div class="header-buttons">
+          <a href="#">About Us</a>
+          <a href="#">Contact Us</a>
+        </div> 
+      </div>
+    </section>
+	 </div>
+              <!--******************************header part is end***********************************-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
